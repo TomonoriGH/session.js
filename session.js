7dfc471fb6d91cee6a564d3e22b5d04972abef64
fsatoshi0427 <fsatoshi0427@yahoo.co.jp>
2025-03-11 14:58:23 +0900

    commit

--- session.js
+++ session.js
@@ -69,7 +69,8 @@ export default function Session() {
     }
 
     this.ImportFromHeaderString = function(str,options){
-        for(const [k,v] of str.split(/; */).map(e => e.split("="))){
+        // for(const [k,v] of str.split(/; */).map(e => e.split("="))){
+        for(const [k,v] of str.split(/; */).map(e => e.split(/(?<=^[^=]*?)=/))){
             const dict = {}
             dict.name = k;
             dict.value = v;
@@ -82,7 +83,7 @@ export default function Session() {
 
     this.ImportFromSetCookieStrings = function(strs,options){
         for(const str of strs){
-            const kvs = str.split("; ").map(e => [e.split("=")[0],e.split("=").slice(1).join("")])
+            const kvs = str.split("; ").map(e => e.split(/(?<=^[^=]*?)=/))
             const dict = {}
             dict.name = kvs[0][0]
             dict.value = kvs[0][1]
